0a1f52f7a0e1c77b01e03096bd96a115018419ce
Igor Crevar <crewce@gmail.com>
2024-11-18 13:12:36 +0100

    APEX-375 ExecuteWithRetry library func refactor

--- e2e-polybft/cardanofw/test-apex-utils.go
+++ e2e-polybft/cardanofw/test-apex-utils.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -15,6 +16,7 @@ import (
 	"testing"
 	"time"
 
+	infracommon "github.com/Ethernal-Tech/cardano-infrastructure/common"
 	"github.com/Ethernal-Tech/cardano-infrastructure/wallet"
 	"github.com/stretchr/testify/require"
 )
@@ -217,7 +219,37 @@ type OracleStateResponse struct {
 }
 
 func IsRecoverableError(err error) bool {
-	return strings.Contains(err.Error(), "status code 500")
+	// Context was explicitly canceled or deadline exceeded; not retryable
+	if infracommon.IsContextDoneErr(err) {
+		return false
+	}
+
+	var netErr net.Error
+	if errors.As(err, &netErr) {
+		return true
+	}
+
+	if errors.Is(err, infracommon.ErrRetryTryAgain) {
+		return true
+	}
+
+	retryableMessages := []string{
+		"replacement tx underpriced",
+		"nonce too low",
+		"intrinsic gas too low",
+		"tx with the same nonce is already present",
+		"rejected future tx due to low slots",
+		"status code 500", // retry if error is ogmios "status code 500"
+	}
+	errStr := err.Error()
+
+	for _, msg := range retryableMessages {
+		if strings.Contains(errStr, msg) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func GetNetworkMagic(networkType wallet.CardanoNetworkType) uint {
